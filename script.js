e4389b96387c3d576aeacca40906efdb45758039
EricchiYukia <git@ericchi.moe>
2022-11-15 16:20:24 +0100

    Finita la base

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-let listaspesa = ['Squame di drago', 'Occhi di lucertola', 'Fegato di mandragora', 'Spore di fungo magico', 'Bacchetta di mogano', 'Almanacco delle evocazioni"', 'Pietra dell\'anima', 'Dolcetti di cioccolato'];
+const listaSpesa = ['Squame di drago', 'Occhi di lucertola', 'Fegato di mandragora', 'Spore di fungo magico', 'Bacchetta di mogano', 'Almanacco delle evocazioni"', 'Pietra dell\'anima', 'Dolcetti di cioccolato'];
+let listaUl = document.getElementById("contieniElementi");
+let contaElementi = 0;
+let htmlLiClass ='list-group-item d-flex justify-content-between align-items-center'
+let htmlLi
 
-for (let i=0; i<listaspesa.length, i++) {
-    console.log("For: " + listaspesa[i]);
+for (let i=0; i<listaSpesa.length; i++) {
+    console.log("For: " + listaSpesa[i]);
 }
 
 let i=0;
-while (i<listaspesa.length) {
-    console.log("While: " + listaspesa[i]);
+while (i<listaSpesa.length) {
+    console.log("While: " + listaSpesa[i]);
+    listaUl.innerHTML += '<li id="e' + contaElementi + '" class="' + htmlLiClass + '">' + listaSpesa[i] + '</li>';
     i++;
 }
 
-function func() {
-    
+function aggiungi() {
+    let elementoNuovo = (document.getElementById("inputLista").value);
+    contaElementi++;
 }
