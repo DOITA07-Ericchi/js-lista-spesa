02f8bc6a5c59656d32c6a3a10033cf61d039393a
EricchiYukia <git@ericchi.moe>
2022-11-15 15:53:32 +0100

    Base fatta

---
+++ script.js
@@ -0,0 +1,15 @@
+let listaspesa = ['Squame di drago', 'Occhi di lucertola', 'Fegato di mandragora', 'Spore di fungo magico', 'Bacchetta di mogano', 'Almanacco delle evocazioni"', 'Pietra dell\'anima', 'Dolcetti di cioccolato'];
+
+for (let i=0; i<listaspesa.length, i++) {
+    console.log("For: " + listaspesa[i]);
+}
+
+let i=0;
+while (i<listaspesa.length) {
+    console.log("While: " + listaspesa[i]);
+    i++;
+}
+
+function func() {
+    
+}
