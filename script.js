27d3dbdf02119cb672121be94cc3b81103ee1a23
Ericchi <git@ericchi.moe>
2022-11-15 17:32:56 +0100

    Finito!

--- script.js
+++ script.js
@@ -2,17 +2,20 @@ const listaSpesa = ['Squame di drago', 'Occhi di lucertola', 'Fegato di mandrago
 let listaUl = document.getElementById("contieniElementi");
 let contaElementi = 0;
 let htmlLiClass ='list-group-item d-flex justify-content-between align-items-center'
-let htmlLiRemoverClass ='badge bg-danger rounded-pill'
+let htmlLiRemoverClass ='badge bg-danger rounded-pill cursor-pointer'
 
+// Aggiungitorizzatore incantato di elementi
+function nuovoElemento(elemento, indice) {
+    listaUl.innerHTML += '<li id="e' + indice + '" class="' + htmlLiClass + '">' + elemento + '<span onclick="rimuovi(`e' + indice +'`)" class="' + htmlLiRemoverClass + '">X</span></li>';
+    contaElementi++;
+}
+
+// For
 for (let i=0; i<listaSpesa.length; i++) {
     console.log("For: " + listaSpesa[i]);
 }
 
-function nuovoElemento(elemento, indice) {
-    listaUl.innerHTML += '<li id="e' + indice + '" class="' + htmlLiClass + '">' + elemento + '<span id="x' + indice + '" onclick="rimuovi(e' + indice +')" class="' + htmlLiRemoverClass + '">X</span></li>';
-    contaElementi++;
-}
-
+// While
 let i=0;
 while (i<listaSpesa.length) {
     console.log("While: " + listaSpesa[i]);
@@ -20,11 +23,13 @@ while (i<listaSpesa.length) {
     i++;
 }
 
+// Aggiunta e rimozione
 function aggiungi() {
     let aggiungiNuovo = (document.getElementById("inputLista").value);
     nuovoElemento(aggiungiNuovo, contaElementi);
 }
 
 function rimuovi(elemento) {
-    document.getElementById("" +elemento).remove;
+    console.log(elemento + " rimosso.");
+    document.getElementById(elemento).remove();
 }
